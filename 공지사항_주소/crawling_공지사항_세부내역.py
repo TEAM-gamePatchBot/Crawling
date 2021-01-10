a4b7ef106c85166b4eee2e22239712c4e1c2243a
Sang-YunChi <csy960407@naver.com>
2021-01-10 21:16:28 +0900

    Create crawling_공지사항_세부내역.py

---
+++ 공지사항_주소/crawling_공지사항_세부내역.py
@@ -0,0 +1,18 @@
+# 라이브러리 불러오기
+import requests
+from bs4 import BeautifulSoup
+
+# 검색 키워드
+
+# 해당 url의 html문서를 soup 객체로 저장
+url='https://kart.nexon.com/Kart/News/Patch/view.aspx?n4articlesn=75185'
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+
+search_result = soup.select('#kart_main_sections> section.board > div.board_inner')
+
+for i in search_result:
+    print(i.text)
+    
+#수정중 깃데스크탑 배우는
